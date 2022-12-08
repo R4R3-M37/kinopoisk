77773927ba186aba2b43596fb08bfa291cb55c2c
R4R3-M37 <88512151+R4R3-M37@users.noreply.github.com>
2022-12-08 23:57:06 +0500

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,4 @@
 module.exports = {
 	presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript', 'atomic-router/babel-preset'],
-	plugins: [[
-		'effector-logger/babel-plugin',
-		{
-			'inspector': true,
-			'effector': {
-				'factories': ['@farfetched/core', '@farfetched/react']
-			}
-		}
-	],
-	'effector/babel-plugin']
+	plugins: ['effector/babel-plugin']
 }
